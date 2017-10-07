9f2e982a2cae86730a784c59feedd12398093e4b
Alfon <alfon.linux@gmail.com>
2017-10-07 18:36:07 +0200

    calcular la distanacia entre el usuario y las paradas, devuelve una lista ordenada de menor distancia a mayor distancia

---
+++ src/calculate_distance.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#****************************************************************************#
+# Project: Smart Jump                                                        #
+# author: nixijav <nixijav@nixijav.com>                                      #
+# author: alfonso bilbao velez <alfon.linux@gmail.com>                       #
+# Developed: Hackatinho Mobilidade Sostible                                  #
+# License: AGPL                                                              #
+#****************************************************************************#
+
+# MODULO CALCULATE DISTANCE #
+# Librerias #
+import json
+import operator
+from math import acos
+from math import cos
+from math import sin
+from math import radians
+
+
+def haversine(lat_parada, lon_parada, lat_usuario, lon_usuario):
+    distancia = 6371000 * acos(cos(radians(lat_parada)) * cos(radians(lat_usuario)) *\
+    cos(radians(lon_usuario) - radians(lon_parada)) +\
+    sin(radians(lat_parada)) * sin(radians(lat_usuario)))
+
+    return distancia
+
+def calculate_distance(lat_usuario, lon_usuario):
+    jumps = open("dataset/get_jumps.json", "r")
+    jumps_data = json.load(jumps)
+
+    data_out = []
+    for i,elem in enumerate(jumps_data):
+        distancia = (haversine(float(elem['lat']),float(elem['lon']), lat_usuario ,\
+            lon_usuario ))
+        nome = elem['assetName']
+        ids = elem['assetId']
+        bicis = elem['available']
+        data_out.append([ids, distancia, "bike", nome, bicis])
+
+    #print(d)
+    data_out.sort(key=operator.itemgetter(1))
+
+    return data_out
+
+
