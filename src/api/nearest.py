0855c603aa41479668d22b9e7329ee11009fd7f7
Rafa Couto <caligari@treboada.net>
2017-10-07 16:02:44 +0200

    Dummy API: /nearest.

---
+++ src/api/nearest.py
@@ -0,0 +1,58 @@
+
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+@app.route('/nearest', methods=['POST'])
+def hello_world():
+
+    if request.method == 'POST' and request.is_json:
+
+        latitude = request.json['latitude']
+        longitude = request.json['longitude']
+
+        jumps = nearest_jumps(latitude, longitude)
+        return json.dumps(jumps);
+
+    return 'It must be method=POST and mimetype=application/json'
+
+
+def nearest_jumps(latitude, longitude):
+
+    jumps = [
+                {
+                    'id': 76,
+                    'latitude': 43.3674647,
+                    'longitude': -8.438327,
+                    'distance': 290,
+                    'mean': "bus",
+                    'info': {
+                        'address': "Ronda de Outeiro, 54"
+                        }
+                    },
+				{ 
+					'id': 34,
+					'latitude': 43.3848483,
+					'longitude': -8.433362,
+					'distance': 434,
+					'mean': "bike",
+					'info': {
+						'address': "Estación de trenes"
+						}
+					},
+				{ 
+					'id': 77,
+					'latitude': 43.383848,
+					'longitude': -8.433838,
+					'distance': 560,
+					'mean': "bus",
+					'info': {
+						'address': "Alfonso Molina, 112"
+						}
+					}
+            ]
+
+    return jumps
+
