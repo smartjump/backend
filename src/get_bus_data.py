7bdc245d92b8dd59cf90a73fbaaa4519e6c7847e
Alfon <alfon.linux@gmail.com>
2017-10-08 00:11:37 +0200

    recoger la informacion  que tienen cada parada

--- src/get_bus_data.py
+++ src/get_bus_data.py
@@ -15,6 +15,7 @@ import json
 
 # Ficheros #
 f1 = open("dataset/ubicacion_paradas_bus.json", "w")
+f2 = open("dataset/info_por_parada.json")
 
 # Header #
 headers = {'x-sofia2-apikey': "14b0fdf8d58541a087c49a97f61903eb",
@@ -25,10 +26,18 @@ headers = {'x-sofia2-apikey': "14b0fdf8d58541a087c49a97f61903eb",
 """Fetch backstations."""
 def ubicacion_paradas():
     ubicacion_paradas = "https://smart.coruna.es/sib-api/api/v1/openDataParadaAutobus/getAssets"
-    p1 = requests.get(ubicacion_paradas, headers=headers)
+    u1 = requests.get(ubicacion_paradas, headers=headers)
+    u1_json = u1.json()
+    print(json.dumps(json.loads(u1_json['data'])), file=f1)
+    # json.dump(f1, json.loads(u1_json['data']))
+
+
+# Toda la info de una parada de autobus urbano.
+def info_parada():
+    info_parada = "https://smart.coruna.es/sib-api/api/v1/openDataParadaAutobus/getAssetInfo?$id=1"
+    p1 = requests.get(info_parada, headers=headers)
     p1_json = p1.json()
-    print(json.dumps(json.loads(p1_json['data'])), file=f1)
-    # json.dump(f1, json.loads(p1_json['data']))
+    print(json.dumps(json.loads(p1_json['data'])), file=f2)
 
 
 def main():
